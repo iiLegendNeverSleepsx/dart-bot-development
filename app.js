00552ea8fce5be42f8afd525217a53e40b3f7f7c
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-19 23:38:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,13 +14,24 @@ client.on('message', async msg => {
     const m = await msg.channel.send("Pinging...");
     m.edit(`Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
-  if (command === 'hi') {
+  if (command === 'hi' || command = 'hello') {
     msg.reply('bye!');
   }
-  if (command === 'say') {
+  if (command === 'say' || command === "send") {
     const allowid = [0];
     const sayMessage = args.join(" ");
     msg.channel.send(sayMessage);
+  }
+  
+  if(command === "purge" || command === "clean" || command = "clear") {
+    const deleteCount = parseInt(args[0], 10);
+    
+    if(!deleteCount || deleteCount < 2 || deleteCount > 10000)
+      return message.reply("Please provide a number between 2 and 10,000 for the number of messages to delete");
+    
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
 });
 
