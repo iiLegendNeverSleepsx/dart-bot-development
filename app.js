a8885ef601b9e1192e2339ca7375d2280694f5d3
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-20 16:00:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,35 @@ client.on('message', async msg => {
   }
   
   if (command === 'help' || command === 'commands') {
-    msg.reply("In development!")
+    msg.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "Commands",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines.",
+        value: "h"
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
   }
   
   if (command === 'slowmode') {
