2b3342a6a9a1c1ee215429f98186e9a971284b20
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-19 23:22:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+let prefix = ";";
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  const args = msg.content.trim().split(/ +/g);
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   if (command === 'ping') {
     msg.reply('pong!');
