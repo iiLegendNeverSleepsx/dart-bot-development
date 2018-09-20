d3bee7716617b92d484e32f4f686d63590032596
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-19 23:35:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ client.on('message', async msg => {
   const command = args.shift().toLowerCase();
   if (command === 'ping') {
     const m = await msg.channel.send("Pinging...");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    m.edit(`Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   if (command === 'hi') {
     msg.reply('bye!');
