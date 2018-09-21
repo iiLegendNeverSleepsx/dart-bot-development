4e309b0b51c9344dcabf5d0f30eed343adf19121
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-20 14:27:02 -1200

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ fs.readdir("./commands", (err, files) => {
 	let jsfile = files.filter(f => f.split(".").pop() === "js")
 	if (jsfile.length <= 0) {
 		console.log("Couldn't find any commands!")
-		return;
+		return
 	}
 
-	jsfile.forEach((f, i) = > {
+	jsfile.forEach((f, i) => {
 		let props = require(`./commands/${f}`);
 		console.log(`${f} has been loaded!`);
 		client.commands.set(props.help.name, props);
-	})
+	});
 });
 
 client.on("ready", async () => {
