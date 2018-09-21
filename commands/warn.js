9f359287ad59798d01b95193f30f1468cfc793a5
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-20 14:24:27 -1200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -4,10 +4,10 @@ const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("../infractions.json","utf8"));
 
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("it doesn't look like you can use that!")
-	let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+	if (!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("it doesn't look like you can use that!");
+	let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 	if (!wUser) return message.reply(" that member can't be found!");
-	if (wUser.hasPermission("MANAGE_MESSAGES") || wUser.hasPermission("MANAGE_MEMBERS")) return message.reply("can't warn that user!")
+	if (wUser.hasPermission("MANAGE_MESSAGES") || wUser.hasPermission("MANAGE_MEMBERS")) return message.reply("can't warn that user!");
 	let reason = args.join(" ").slice(22);
 
 	if (!warns[wUser.id]) warns[wUser.id] = {
@@ -28,7 +28,7 @@ module.exports.run = async (bot, message, args) => {
 	.addField("Reason:", reason)
 	.addField("Number of Infractions:", warns[wUser.id].warns)
 
-	message.channel.send(warnEmbed)
+	message.channel.send(warnEmbed);
 }
 
 module.exports.help = {
