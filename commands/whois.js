a723e95a0f569fe8b7b35b250962979ebd61be7e
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-21 02:36:54 -0700

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -2,18 +2,16 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
-    const whoisuser = message.mentions.members.first();
+    const whoisuser = message.mentions.members.first() || message.guild.members.get(args[0]);
     const embed = new Discord.RichEmbed()
   .setTitle("Who Is")
   .setAuthor("Dart Bot", bot.user.avatarURL)
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
   .setColor("#00AE86")
   .setDescription("Whois command")
   .setFooter("Requested", "http://i.imgur.com/w1vhFSR.png")
   .setThumbnail(whoisuser.avatarURL)
   .setTimestamp()
+    
   .addField("Name", whoisuser.name, true)
   .addField("Discriminator", whoisuser.discriminator, true)
   .addField("ID", whoisuser.ID)
