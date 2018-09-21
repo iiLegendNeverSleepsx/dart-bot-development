53c1d46d6e564a4208f3abae618d32c5019867fa
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-20 17:13:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ client.on('message', async msg => {
   if (command === 'setstatus' || command === 'ss') {
     const status = args.join(" ");
     client.user.setStatus(status);
-    msg.channel.send(`New status set to: **${status}**.`)
+    msg.channel.send(`New status set to: **${status}**.`);
   }
 });
 
