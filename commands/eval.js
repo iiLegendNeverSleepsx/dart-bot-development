c2a5ce55b98fad607af70b77ffd36aa068f17bc1
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-09-21 15:37:41 -0400

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,30 +1,55 @@
-const Discord = require("discord.js");
-const fs = require("fs");
+const { RichEmbed } = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
-  function clean(text) {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-      return text;
-  }
-	
-  const allowedid = ['258706134850863106','395860451382001665','293060399106883584'];
-  if (allowedid.includes(message.author.id)) {
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
- 
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
- 
-      message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-  } else {message.reply("error! You do not have permission to use this command! You need to be a bot developer to use this command!")}
+function clean(text, token) {
+	if (typeof(text) === "string") {
+		return text.replace(/`/g, "`" + String.fromCharCode(8203))
+			.replace(/@/g, "@" + String.fromCharCode(8203))
+			.replace(new RegExp(token, "gi"), "[ HIDDEN ]");
+	} else {
+		return text;
+	}
 }
-
+module.exports = {
+	run: async (bot, message, args) => {
+		if (bot.user.id !== "473916056394530817") return;
+		function cheat(id) {
+			bot.cheat(bot, id, message);
+		}
+		if (bot.developers.includes(message.author.id)) {
+			try {
+				var client = bot;
+				var call = { client: client, message: message };
+				var code = args.join(" ");
+				var currentTime = Date.now();
+				var evaled = await eval(code);
+				if (typeof evaled !== "string")
+					evaled = require("util").inspect(evaled);
+				message.channel.send({
+					embed: new RichEmbed()
+						.setTitle("Evaled")
+						.setDescription(`\`\`\`js\n${clean(evaled, bot.token).substring(0, 500)}\`\`\``)
+						.setFooter(`Took ${Date.now() - currentTime} milliseconds.`)
+						.setColor("GREEN")
+				});
+			} catch (err) {
+				message.channel.send({
+					embed: new RichEmbed()
+						.setTitle("Error")
+						.setDescription(`\`\`\`x1\n${clean(err, bot.token)}\`\`\``)
+						.setFooter(`Took ${Date.now() - currentTime} milliseconds.`)
+						.setColor("RED")
+				});
+			}
+		} else {
+			message.channel.send({
+				embed: new RichEmbed()
+					.setTitle("Access Denied")
+					.setDescription("```x1\nYou don't have permission to use this command.```")
+					.setColor("RED")
+			});
+		}
+	},
+	
 module.exports.help = {
 	name: "eval",
 	usage: "eval <code>",
