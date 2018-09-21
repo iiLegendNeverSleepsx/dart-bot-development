d4cb44d55de670e40d8006d413b380fe17deb6d2
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-20 14:36:14 -1200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./infractions.json","utf8"));
 
 module.exports.run = async (bot, message, args) => {
