ffad4fa46e69220f7b3be866dc539fd8daf65827
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-21 01:40:27 -0700

    Create nick.js

---
+++ commands/nick.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission("MANAGE_NICKNAMES") || !message.member.hasPermission("ADMINISTRATOR"))
+      return message.reply("it doesn't look like you can use that.");
+    
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot nickname this user! Do they have a higher role? Do I have nickname permissions?");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "nil; aE00o6a2i1";
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`sorry ${message.author}, I couldn't kick because of : ${error}`));
+    message.channel.send(`${member.user.tag}'s nickname successfully set to **${reason}**!`);
+}
+
+module.exports.help = {
+	name: "nick",
+	usage: "nick <user> <nickname>",
+	description: "nil",
+	longdes: "Nicknames the user given to the nickname given.",
+	mentionedperm: "MANAGE_NICKNAMES",
+  category: "Moderation"
+}
