faea8dfec1aea2ec4859dec8e74be9cbbe9200da
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-20 16:47:43 -1200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,16 @@
+const Discord = require("discord.js");
+const fs = require("fs");
 
+module.exports.run = async (bot, message, args) => {
     const m = await msg.channel.send("Pinging...");
     m.edit(`Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+}
+
+module.exports.help = {
+	name: "ping",
+	usage: "ping",
+	description: "pings the bot",
+	longdes: "Pings the bot.",
+	mentionedperm: "None",
+  category: "Utility"
+}
