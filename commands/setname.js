afed17ab42c6aaa7aa4addda59b14c00f975fbf4
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-21 01:46:26 -0700

    Update setname.js

--- commands/setname.js
+++ commands/setname.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     const allowedid = ['258706134850863106','395860451382001665','293060399106883584'];
     if (allowedid.includes(message.author.id)) {
 	    bot.user.setUsername(args.join(" "));
-	    message.reply(`new username set to **${args.join(" ")}`)
+	    message.reply(`new username set to **${args.join(" ")}**`)
     }
 }
 
