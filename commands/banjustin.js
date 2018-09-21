af7da32256c3158ef95fb3a4c6d747b8345a2cb9
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-20 17:34:05 -1200

    Update banjustin.js

--- commands/banjustin.js
+++ commands/banjustin.js
@@ -1,1 +1,16 @@
+const Discord = require("discord.js");
+const fs = require("fs");
 
+module.exports.run = async (bot, message, args) => {
+    const m = await message.channel.send("**Banning `justin#1337...**");
+    m.edit("**justin#1337** has been banned.");
+}
+
+module.exports.help = {
+	name: "ban justin",
+	usage: "banjustin",
+	description: "none",
+	longdes: "Bans the user 'justin#1337'",
+	mentionedperm: "None",
+  category: "Secret"
+}
