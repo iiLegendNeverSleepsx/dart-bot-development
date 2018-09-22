d78a94854951092f20f96175326c0cb9f0850e3a
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-09-22 17:13:04 -0400

    Create restart.js

---
+++ commands/restart.js
@@ -0,0 +1,17 @@
+module.exports.run = async (bot, message, args) => {
+    const allowedid = ['258706134850863106','395860451382001665','293060399106883584'];
+    if (allowedid.includes(message.author.id)) {
+        message.react("✅")
+            .then(() => bot.destroy())
+            .then(() => bot.run());
+    }
+}
+
+module.exports.help = {
+    name: "restart",
+    usage: "restart",
+    description: "nil",
+    longdes: "Restarts the bot.",
+    mentionedperm: "DEVELOPER",
+  category: "Developer"
+}
