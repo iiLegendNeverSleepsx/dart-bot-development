9350d0b30520d21a8cfa70408a2c860263b822c1
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-09-23 01:49:32 +0000

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,11 +1,12 @@
+const index = require(../index.js)
 module.exports.run = async (bot, message, args) => {
     const allowedid = ['258706134850863106','395860451382001665','293060399106883584'];
      if (allowedid.includes(message.author.id)) {
-         message.react("✅")
-             .then(() => bot.destroy()
-             .then(() => bot.login(process.env.BOT_T0KEN)));
-    }
-}
+         	message.react("✅")
+			.then(() => bot.destroy())
+			.then(() => index.run());
+	}
+};    
 
 module.exports.help = {
     name: "restart",
