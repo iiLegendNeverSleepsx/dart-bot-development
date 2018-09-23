2599327eebd7151232df423772f4c88b35271af3
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-24 03:46:40 +0500

    Added "m" to "message"... :facepalm:

--- commands/donate.js
+++ commands/donate.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const fs = require("fs"); 
 
 module.exports.run = async (bot, message, args) => {
-    essage.channel.send({embed: {
+    message.channel.send({embed: {
     color: 3447003,
     author: {
       name: client.user.username,
