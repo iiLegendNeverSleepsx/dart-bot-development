402d3063f42fc3d67ae35e879bb0ed6e87a58b73
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-24 03:44:36 +0500

    Update donate.js

--- commands/donate.js
+++ commands/donate.js
@@ -1,1 +1,34 @@
-// Command: ;donate
+const Discord = require("discord.js");
+const fs = require("fs"); 
+
+module.exports.run = async (bot, message, args) => {
+    essage.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "Donate to us!",
+    description: "Donating to us can help us improve the bot and make it better! You can also get cool features.",
+    fields: [{
+        name: "Patreon",
+        value: "You may donate to us at Patreon [here](https://www.patreon.com/dartbot) - https://www.patreon.com/dartbot."
+      },
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "By SGII2, Lxphere, and Dart"
+    }
+  }
+});
+}
+
+module.exports.help = {
+	name: "donate",
+	usage: "donate",
+	description: "nil",
+	longdes: "Donate to Dart Bot! : )",
+	mentionedperm: "None",
+  category: "Premium"
+}
