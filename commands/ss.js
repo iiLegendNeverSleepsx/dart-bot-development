b90edb03d268c320cb28c1bf024b4bc15725b62c
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-09-24 15:58:10 -0400

    Update ss.js

--- commands/ss.js
+++ commands/ss.js
@@ -2,11 +2,14 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
+  const allowedid = ['258706134850863106','395860451382001665','293060399106883584'];
+  if (allowedid.includes(message.author.id)) {
       bot.user.client.user.setPresence({ game: { type: args[1].toUpperCase(), name: args.splice(2, args.length).join(" ") }, status: args[0].toLowerCase() }).then(() => {
-			message.reply(":white_check_mark: All set!")
-      }).catch(() => {
-			message.reply(":x: Couldn't change the status!")
-})
+			message.reply(":white_check_mark: All set!").catch(() => bot.safeSend(message, module.exports.help.name));
+		}).catch(() => {
+			message.reply(":x: Couldn't change the status!").catch(() => bot.safeSend(message, module.exports.help.name));
+		});
+	},
 
 module.exports.help = {
 	name: "ss",
