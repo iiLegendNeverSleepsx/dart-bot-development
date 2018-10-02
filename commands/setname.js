c657930dec64dbc5e32756f786ced0194a04f502
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-10-03 04:40:01 +0500

    Update setname.js

--- commands/setname.js
+++ commands/setname.js
@@ -4,8 +4,11 @@ const fs = require("fs");
 module.exports.run = async (bot, message, args) => {
     const allowedid = ['258706134850863106','395860451382001665','293060399106883584'];
     if (allowedid.includes(message.author.id)) {
+	    let olduser = bot.user.username;
+	    let newuser = args.join(" ");
 	    bot.user.setUsername(args.join(" "));
-	    message.reply(`new username set to **${args.join(" ")}**`)
+	    message.reply(`new username set to **${args.join(" ")}**`);
+	    bot.guilds.get('489367652410589185').channels.get('496823703988994048').send(`My username has been changed from **${olduser}** to **${newuser}** by **${message.author.tag}**.`)
     }
 }
 
