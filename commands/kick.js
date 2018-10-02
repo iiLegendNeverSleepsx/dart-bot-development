70318593c75e55264de9da7a611491c9dc50d352
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-10-02 17:21:08 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,19 +3,19 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("KICK_MEMBERS") || !message.member.hasPermission("ADMINISTRATOR"))
-      return message.reply("it doesn't look like you can use that.");
+      return message.reply("insufficent permissions! You need to have the `KICK_MEMBERS` permission!");
     
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!member)
-      return message.reply("please mention a valid member of this server");
+      return message.reply("please mention a valid member of this server!");
     if(!member.kickable) 
       return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
     
     let reason = args.slice(1).join(' ');
-    if(!reason) reason = "no reason provided";
+    if(!reason) reason = "No reason provided";
     
     await member.kick(reason)
-      .catch(error => message.reply(`sorry ${message.author}, I couldn't kick because of : ${error}`));
+      .catch(error => message.reply(`sorry ${message.author}, I couldn't kick because of: ${error}`));
     message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 }
 
