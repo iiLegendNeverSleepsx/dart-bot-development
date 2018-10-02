371e8c8b98f8b899ad89f11e3c3d76d108f1a8d6
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-10-02 17:19:10 -0400

    error messages updated

--- commands/ban.js
+++ commands/ban.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("BAN_MEMBERS") || !message.member.hasPermission("ADMINISTRATOR"))
-      return message.reply("error! You do not have permission to use this command! You need the `BAN_MEMBERS` permission!");
+      return message.reply("insufficent permissions! You need to have the `BAN_MEMBERS` permission!");
     
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!member)
-      return message.reply("please mention a valid member of this server");
+      return message.reply("please mention a valid member of this server!");
     if(!member.kickable) 
       return message.reply("I cannot ban this user! Do they have a higher role? **Do I have ban permissions**?");
     
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
     if(!reason) reason = "no reason provided";
     
     await member.ban(reason)
-      .catch(error => message.reply(`sorry ${message.author}, I couldn't ban because of : ${error}`));
+      .catch(error => message.reply(`sorry ${message.author}, I couldn't ban because of: ${error}`));
     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
 }
 
