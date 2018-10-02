9d5a1d0ce04c2fde015807d38cd81511aaf6243c
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-10-03 04:44:35 +0500

    Messages have proper grammar and punctuation now

--- commands/setname.js
+++ commands/setname.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
 	    let olduser = bot.user.username;
 	    let newuser = args.join(" ");
 	    bot.user.setUsername(args.join(" "));
-	    message.reply(`new username set to **${args.join(" ")}**`);
+	    message.reply(`new username set to **${args.join(" ")}**!`);
 	    bot.guilds.get('489367652410589185').channels.get('496823703988994048').send(`My username has been changed from **${olduser}** to **${newuser}** by **${message.author.tag}**.`)
     }
 }
