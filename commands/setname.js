c83c3ac4e1744ce32baaffe5d48cf3bdc61cffaa
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-10-03 05:01:55 +0500

    Fixed spelling issues

--- commands/setname.js
+++ commands/setname.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
 	    let olduser = bot.user.username;
 	    let newuser = args.join(" ");
 	    bot.user.setUsername(args.join(" ")).then(() => {message.reply(`new username set to **${args.join(" ")}**!`); bot.guilds.get('489367652410589185').channels.get('496823703988994048').send(`My username has been changed from **${olduser}** to **${newuser}** by **${message.author.tag}**.`)})
-	    	.catch((error) => {message.reply(`can not set username to **${newuser}** because of \`${error}\``); bot.guilds.get('489367652410589185').channels.get('496823703988994048').send(`Set username command ran by**${message.author.tag}** failed with error: \`${error}\`.`)})
+	    	.catch((error) => {message.reply(`can not set username to **${newuser}** because of \`${error}\``); bot.guilds.get('489367652410589185').channels.get('496823703988994048').send(`Set username command ran by **${message.author.tag}** failed with error: \`${error}\`.`)})
 	    
     }
 }
