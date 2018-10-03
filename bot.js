bed7556f7f4f349cda1c8f473c3463056bd66704
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-10-03 05:40:22 +0500

    attempting prompt for SS

--- bot.js
+++ bot.js
@@ -3,6 +3,11 @@ const client = new Discord.Client();
 
 const fs = require("fs")
 
+let sspo = false;
+let sspuid = 0;
+let ssps = 0;
+let sspod = false;
+
 client.commands = new Discord.Collection();
 
 fs.readdir("./commands", (err, files) => {
@@ -36,13 +41,67 @@ client.on("message", async message => {
 	
   	if(!prefix) return;
 	
-	
 	let messageArray = message.content.split(" ");
 	let cmd = messageArray.shift();
 	let args = messageArray
 
+	if (cmd.slice(prefix.length) === 'ss') {
+		const allowids = ['293060399106883584', '395860451382001665', '258706134850863106'];
+		if (allowids.includes(message.author.id)) {
+			sspo = true;
+			sspuid = message.author.id;
+			ssps = 0;
+		}
+	}
+	
+	if (sspo === true && ssuid === message.author.id) {
+		if (sspod === true) {ssps = 2}
+		if (ssps === 1) {
+			sspod = true
+			message.channel.send({embed: {
+    				color: 15844367,
+    				description: "\n",
+    				fields: [{
+    				    name: "Set New Status",
+   				    value: "What **game** would you like my status to be?"
+ 		 		    },
+  				],
+				}
+			});
+		}
+		
+		if (ssps === 2) {
+			const allowedans = ['online','idle','dnd','invisible'];
+			if (!allowedans.includes(args[0])) {
+			message.channel.send({embed: {
+    				color: 15844367,
+    				description: "\n",
+    				fields: [{
+    				    name: "Set New Status",
+   				    value: "What **state** would you like me to be in? Please say `online`, `idle`, `dnd`, or `invisible`."
+ 		 		    },
+  				],
+				}
+			});
+			} else {
+			message.channel.send({embed: {
+    				color: 130255146,
+    				description: "\n",
+    				fields: [{
+    				    name: "Set New Status",
+   				    value: "Successfuly set new status!"
+ 		 		    },
+  				],
+				}
+			});
+			}
+		}
+	} else {
+	
 	let commandfile = client.commands.get(cmd.slice(prefix.length));
 	if (commandfile) commandfile.run(client,message,args);
+	
+	}
 });
 
 client.login(process.env.BOT_T0KEN);
