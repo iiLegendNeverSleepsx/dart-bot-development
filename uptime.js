903179151d8332aa2c66ddc5003aaecb263e6185
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-10-05 15:37:41 -0400

    Create uptime.js

---
+++ uptime.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const fs = require("fs"); 
+
+module.exports.run = async (bot, message, args) => {
+    const client = bot;
+    
+    let totalSeconds = (client.uptime / 1000);
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = Math.round(totalSeconds % 60);
+    
+    let uptime = `${hours}h ${minutes}m ${seconds}s`;
+    message.channel.send({embed: {
+    color: 15844367,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "Uptime",
+    description: "\n",
+    fields: [{
+        name: "Uptime",
+        value: uptime,
+        inline: true
+      },
+  ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: `${message.author.avatarURL}`,
+      text: `Requested by ${message.author.tag}`
+    }
+  }
+});
+}
+
+module.exports.help = {
+	name: "uptime",
+	usage: "uptime",
+	description: "nil",
+	longdes: "Uptime of bot.",
+	mentionedperm: "None",
+  category: "Utility"
+}
