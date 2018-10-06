5865f4d9dd578b66c868c4c3b98fec8badebf98b
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-10-05 20:49:37 -0700

    Sudo: added mention arg

--- commands/sudo.js
+++ commands/sudo.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
   //what happens when the command is ran, do it normally.
+  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if (!wUser) return message.reply("that member can't be found!");
+	
+	
 }
 
 module.exports.help = {
