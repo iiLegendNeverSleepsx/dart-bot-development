f4c78215943d3cb347c7ca20fa5b773eb0c67751
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-10-05 20:50:48 -0700

    Sudo: added permissions

--- commands/sudo.js
+++ commands/sudo.js
@@ -3,10 +3,12 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
   //what happens when the command is ran, do it normally.
+  const allowedid = ['258706134850863106','395860451382001665','293060399106883584'];
+  if (allowedid.includes(message.author.id)) {
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if (!wUser) return message.reply("that member can't be found!");
 	
-	
+  }
 }
 
 module.exports.help = {
