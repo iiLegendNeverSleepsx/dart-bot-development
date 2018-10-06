1c180072a7f7cfa8d8e6526e6975825b958ceb81
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-10-05 20:55:11 -0700

    Update sudo.js

--- commands/sudo.js
+++ commands/sudo.js
@@ -2,11 +2,21 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
+  const client = bot;
   //what happens when the command is ran, do it normally.
   const allowedid = ['258706134850863106','395860451382001665','293060399106883584'];
   if (allowedid.includes(message.author.id)) {
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if (!wUser) return message.reply("that member can't be found!");
+	  
+	  let unused = args.shift();
+	  let cmd = args.shift();
+	  let nargs = args;
+	  
+	  message.author = wUser;
+	 
+	let commandfile = client.commands.get(cmd);
+	if (commandfile) commandfile.run(client,message,nargs);
 	
   }
 }
