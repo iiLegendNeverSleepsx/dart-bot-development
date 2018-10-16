4b2520912c7d3f026b0be7f3c68c67bb609505d8
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-10-16 16:17:45 -0400

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -27,7 +27,7 @@ module.exports.run = async (bot, message, args) => {
       },
       {
         name: "Version",
-        value: "v0.5-DEV",
+        value: "V0.5",
         inline: true
       },
       {
