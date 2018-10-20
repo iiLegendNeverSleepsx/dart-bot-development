21a009a4eb4b79695c9dff1f645ab147c8c9e99d
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-10-20 10:59:55 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -8,12 +8,12 @@ module.exports.run = async (bot, message, args) => {
   message.channel.bulkDelete(newcount).then(() => {
   message.channel.send(`Deleted **${count}** messages!`).then(msg => msg.delete(3000));
 });
-  } else {message.reply("error! You do not have permission to use this command! You need the `MANAGE_MESSAGES` permission!")}
+  } else {message.reply("\nError! You do not have permission to use this command! You need the `MANAGE_MESSAGES` permission!")}
 }
 
 module.exports.help = {
 	name: "purge",
-	usage: "purge 15",
+	usage: "purge <number of messages>",
 	description: "A description found in the ;help embed.",
 	longdes: "Purges the amount of messages given. Must be between `2` and `100`.",
 	mentionedperm: "MANAGE_MESSAGES",
