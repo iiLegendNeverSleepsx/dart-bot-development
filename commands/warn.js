f5bd99992b64da689e76a97a7dd0655150737920
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-10-20 11:58:59 -0400

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -5,7 +5,7 @@ let warns = JSON.parse(fs.readFileSync("./infractions.json","utf8"));
 module.exports.run = async (bot, message, args) => {
 	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("it doesn't look like you can use that!");
 	let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-	if (!wUser) return message.reply("that member can't be found!");
+	if (!wUser) return message.reply("\nThat member can't be found!");
 	if (wUser.hasPermission("MANAGE_MESSAGES") || wUser.hasPermission("ADMINISTRATOR")) return message.reply("\nCannot warn that user! Check that I have sufficent permissions, or you have permission to warn that user!");
 	let reaso = args.shift();
 	let reason = args.join(" ");
