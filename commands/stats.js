d2975dd98b939e23b73df70bbcb1e1eb746c775f
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-10-21 13:50:10 -0400

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -5,12 +5,14 @@ module.exports.run = async (bot, message, args) => {
     const client = bot;
     
     let totalSeconds = (client.uptime / 1000);
+    let weeks = Math.floor(totalSeconds / 604800);
+    let days = Math.floor(totalSeconds / 86400);
     let hours = Math.floor(totalSeconds / 3600);
     totalSeconds %= 3600;
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = Math.round(totalSeconds % 60);
     
-    let uptime = `${hours}h ${minutes}m ${seconds}s`;
+    let uptime = `${weeks}w ${days}d ${hours}h ${minutes}m ${seconds}s`;
     
     message.channel.send({embed: {
     color: 15844367,
