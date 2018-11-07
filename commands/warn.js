a33a902bd0aadfe3ba0fc4159dfea5f0f7990de9
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-11-07 15:45:51 -0500

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
 	if (wUser.hasPermission("MANAGE_MESSAGES") || wUser.hasPermission("ADMINISTRATOR")) return message.reply("\nCannot warn that user! Check that I have sufficent permissions, or you have permission to warn that user!");
 	let reaso = args.shift();
 	let reason = args.join(" ");
-
+        message.delete();
 	if (!reason) {reason = "*No reason specified.*"}
         message.channel.send(`**${message.author.username}** has warned <@${wUser.id}> because of **${reason}**.`);
 	wUser.send(`You have been warned in **${message.guild.name}** for **${reason}**.`);
